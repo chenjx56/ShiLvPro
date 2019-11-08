d3aa1a8c4431a7d77b9b2ffb0f3c4a424458f06e
chenjx56 <785206541@qq.com>
2019-11-08 12:45:10 +0800

    第一次尝试合并，完成首页，登录，垃圾分类，用户小信息块的主要功能

--- ShiLv.WebUI/Content/js/Junk.js
+++ ShiLv.WebUI/Content/js/Junk.js
@@ -1,1 +1,96 @@
-﻿
+﻿$('#selectList').change(function () {
+    ajaxForJunks();
+})
+function ajaxForJunks() {
+    $.ajax({
+        url: '../Junk/GetJunksByPage',
+        type: 'post',
+        data: {
+            typeID: $('#selectList').val().trim(),
+            junkName: $('#JunkName').val().trim()
+        },
+        success: function (data) {
+            junkName: $('#JunkName').val('');
+            $('#junksContainer').html(data);
+        }
+    });
+}
+$('.search.bar6 button').click(function () {
+    ajaxForJunks();
+})
+$('.search.bar6').keydown(function (e) {
+    if(e.keyCode == 13){
+        ajaxForJunks();
+    }
+});
+var picString = null;
+$("#picAjax").change(function (e) {
+    var file = e.delegateTarget.files[0];
+    //判断文件
+    var path = $('#picAjax').val();
+    if (path.length == 0) {
+        $('.tips').html("请选择要上传的图片");
+        $('.tips').css("color", "red");
+        return;
+    }
+    var extStart = path.lastIndexOf('.'),
+        ext = path.substring(extStart, path.length).toUpperCase();
+    if (ext !== ".PNG" && ext !== ".JPG" && ext !== ".JPEG") {
+        $('.tips').html('请选择png/jpg/jpeg格式');
+        $('.tips').css("color", "red");
+        return;
+    }
+    var reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = function () {
+        picString = reader.result;
+        $("#selImg").attr({ "src": picString });
+    }
+});
+
+$(function () {
+    $('#btn-up').click(function () {
+        if ($('.user-pic .name').html() == null) {
+            alert('请登录账号后进行操作！');
+            window.location.href = '../UserInfo/Index';
+            return;
+        }
+        if ($('.add-box #selectList').val() == 0) {
+            $('.tips').html('请选择分类！');
+            $('.tips').css("color", "red");
+            return;
+        }
+        if ($('#picAjax').val().length == 0) {
+            $('.tips').html('请选择图片！');
+            $('.tips').css("color", "red");
+            return;
+        }
+        var formData = new FormData($('form')[0]);
+        formData.append("junkName", $('#addName').val());
+        formData.append("junkType", $('.add-box #selectList').val());
+        formData.append("userName", $('.user-pic .name').attr("id"));
+        $.ajax({
+            url: '../Junk/SavePicture',
+            type: 'post',
+            data: formData,
+            xhr: function () {
+                return $.ajaxSettings.xhr();
+            },
+            success: function (data) {
+                console.log(data);
+            },
+            cache: false,
+            contentType: false,
+            processData: false
+        });
+    });
+
+    $('.add-box .icon-cha').click(function () {
+        $('.add-bg').fadeOut(300);
+    })
+    $('.icon-jia').click(function () {
+        $('.add-bg').fadeIn(300);
+    })
+});
+
+
--- ShiLv.WebUI/Controllers/JunkController.cs
+++ ShiLv.WebUI/Controllers/JunkController.cs
@@ -8,6 +8,7 @@ using BLL;
 using System.Web.Script.Serialization;
 using System.Text;
 using PagedList;
+using System.Text.RegularExpressions;
 
 namespace ShiLv.WebUI.Controllers
 {
@@ -17,18 +18,94 @@ namespace ShiLv.WebUI.Controllers
         JunkBLL junkBLL = new JunkBLL();
         JunkTypeBLL junkTypeBLL = new JunkTypeBLL();
         // GET: Junk
-        public ActionResult Index()
+        public ActionResult Index(int ID = 0)
         {
-            var typeList = junkTypeBLL.GetJunkTypeForTop();
-            return View(typeList.ToList());
+            List<JunkType> list = junkTypeBLL.GetJunkTypeForTop().ToList();
+            list.Insert(0, new JunkType() { ID = 0, TypeName = "全部分类" });
+            var selectList = new SelectList(list, "ID", "TypeName", ID);
+            ViewBag.selectList = selectList;
+            return View();
         }
-        public ActionResult Test(int? page)
-        {//, bool jqueryUnobtrusive = false
-            var junks = db.Junks.OrderByDescending(p => p.ID);
+
+        /// <summary>
+        /// 通过查询条件获取垃圾的视图
+        /// </summary>
+        /// <param name="page"></param>
+        /// <param name="typeID"></param>
+        /// <param name="junkName"></param>
+        /// <returns></returns>
+        public ActionResult GetJunksByPage(int? page,int typeID = 0,string junkName = null)
+        {
+            
+            IQueryable<Junks> junks = null;
+            if (typeID == 0)
+            {
+                if (junkName != null)
+                {
+                    junks = db.Junks.OrderByDescending(p => p.ID).Where(p=>p.JunksName.Contains(junkName));
+                }
+                else
+                {
+                    junks = db.Junks.OrderByDescending(p => p.ID);
+                }
+            }
+            else
+            {
+                if (junkName != null)
+                {
+                    junks = db.Junks.OrderByDescending(p => p.ID).Where(p => p.JunkTypeId == typeID && p.JunksName.Contains(junkName));
+                }
+                else
+                {
+                    junks = db.Junks.OrderByDescending(p => p.ID).Where(p => p.JunkTypeId == typeID);
+                }
+                
+            }
+            
             int pageSize = 8;
             int pageNumber = (page ?? 1);
-            //ViewBag.jqueryUnobtrusive = jqueryUnobtrusive;
             return View(junks.ToPagedList(pageNumber, pageSize));
+        }
+        
+        public string SavePicture()
+        {
+            string result = "";
+            HttpPostedFileBase imageName = Request.Files["image"];// 从前台获取文件
+            string junkName = Request["junkName"];
+            int junkType = Convert.ToInt32(Request["junkType"]);
+            string userName = Request["userName"];
+            string file = imageName.FileName;
+            string fileFormat = file.Split('.')[file.Split('.').Length - 1]; // 以“.”截取，获取“.”后面的文件后缀
+            Regex imageFormat = new Regex(@"^(bmp)|(png)|(gif)|(jpg)|(jpeg)"); // 验证文件后缀的表达式
+            if (string.IsNullOrEmpty(file) || !imageFormat.IsMatch(fileFormat)) // 验证后缀，判断文件是否是所要上传的格式
+            {
+                result = "error";
+            }
+            else
+            {
+                string timeStamp = DateTime.Now.Ticks.ToString(); // 获取当前时间的string类型
+                string firstFileName = timeStamp.Substring(0, timeStamp.Length - 4); // 通过截取获得文件名
+                string imageStr = "Content/images/junk/"; // 获取保存图片的项目文件夹
+                string uploadPath = Server.MapPath("~/" + imageStr); // 将项目路径与文件夹合并
+                string pictureFormat = file.Split('.')[file.Split('.').Length - 1];// 设置文件格式
+                string fileName = firstFileName + "." + fileFormat;// 设置完整（文件名+文件格式） 
+                string saveFile = uploadPath + fileName;//文件路径
+                imageName.SaveAs(saveFile);// 保存文件
+                // 如果单单是上传，不用保存路径的话，下面这行代码就不需要写了！
+                string image = "junk/" + fileName;// 设置数据库保存的路径
+                Junks junk = new Junks()
+                {
+                    JunksName = junkName,
+                    JunkImage = image,
+                    JunkTypeId = junkType,
+                    UserName = userName,
+                    LikeNum = 0,
+                    DislikeNum = 0
+                };
+                junkBLL.Add(junk);
+                result = "success";
+            }
+            return result;
         }
     }
 }
